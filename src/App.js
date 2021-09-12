544fc607fcf232c3a24fda98f387742b77aaa26f
Agostina Sancisi <agossancisi@gmail.com>
2021-09-12 19:27:11 -0300

    Aprendiendo a usar React

--- src/App.js
+++ src/App.js
@@ -4,22 +4,19 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello world
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <body>
+        <Title text={"world"}/>
+      </body>
     </div>
   );
+}
+
+const Title = (info) =>{
+  return(
+    <div>
+      <h1>Hello {info.text}</h1>
+    </div>
+  )
 }
 
 export default App;
